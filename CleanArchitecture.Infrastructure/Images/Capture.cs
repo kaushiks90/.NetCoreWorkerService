63287bf63807098c0f1855420d36405a5c63d4e0
kaushiks90 <kaushiks90@gmail.com>
2020-06-07 09:59:30 +0530

    Capture Image multiple Monitors

--- CleanArchitecture.Infrastructure/Images/Capture.cs
+++ CleanArchitecture.Infrastructure/Images/Capture.cs
@@ -13,12 +13,12 @@ namespace CleanArchitecture.Infrastructure.Images
         public void CaptueImage()
         {
 
-            /*
-            Bitmap bmp = null;
-            //pictureBox1.Image = bmp;
-            // window.screen.width
-            //float height = SystemInformation.VirtualScreen.Height;
-            //float width = SystemInformation.VirtualScreen.Width;
+            //Method 1
+            //ScreenCapture sc = new ScreenCapture();
+            //Image img = sc.CaptureScreen();
+            //img.Save("img.png", System.Drawing.Imaging.ImageFormat.Png);
+
+            //Method 2
 
             ManagementObjectSearcher searcher = new ManagementObjectSearcher(@"\root\wmi", @"SELECT * FROM WmiMonitorBasicDisplayParams");
 
@@ -26,34 +26,25 @@ namespace CleanArchitecture.Infrastructure.Images
             foreach (ManagementObject managementObject in searcher.Get())
             {
                 //Calculate monitor size
-                double width = (byte)managementObject["MaxHorizontalImageSize"] / 2.54;
-                double height = (byte)managementObject["MaxVerticalImageSize"] / 2.54;
-                double diagonal = Math.Sqrt(width * width + height * height);
+                //double width = (byte)managementObject["MaxHorizontalImageSize"] / 2.54;
+                //double height = (byte)managementObject["MaxVerticalImageSize"] / 2.54;
+                //double diagonal = Math.Sqrt(width * width + height * height);
 
-                bmp = new Bitmap(Convert.ToInt32(width * width), Convert.ToInt32(height * height));
+                double width = (byte)managementObject["MaxHorizontalImageSize"] ;
+                double height = (byte)managementObject["MaxVerticalImageSize"];
+               // double diagonal = Math.Sqrt(width * width + height * height);
+
+
+                Bitmap bmp = new Bitmap(Convert.ToInt32(width * width), Convert.ToInt32(height * height));
                 Graphics gr = Graphics.FromImage(bmp);
                 gr.CopyFromScreen(0, 0, 0, 0, bmp.Size);
 
 
                 bmp.Save("img.png", System.Drawing.Imaging.ImageFormat.Png);
                 //Output monitor size
-                Console.WriteLine("Monitor Size: {0:F1}\"", diagonal);
+               // Console.WriteLine("Monitor Size: {0:F1}\"", diagonal);
             }
-            */
-            ScreenCapture sc = new ScreenCapture();
-            // capture entire screen, and save it to a file
-            Image img = sc.CaptureScreen();
-            // display image in a Picture control named imageDisplay
-
-            // capture this window, and save it
-            // sc.CaptureWindowToFile(this.Handle, "C:\\temp2.gif", ImageFormat.Gif);
-
-            //Bitmap bmp = new Bitmap(width, 1000);
-            //Graphics gr = Graphics.FromImage(bmp);
-            //gr.CopyFromScreen(0, 0, 0, 0, bmp.Size);
-
-            img.Save("img.png", System.Drawing.Imaging.ImageFormat.Png);
-
+                                 
 
         }
     }
