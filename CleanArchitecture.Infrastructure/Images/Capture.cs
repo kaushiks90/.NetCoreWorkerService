f1f116368b4f8ab4964bcef5e4c386ed88d59bc1
kaushiks90 <kaushiks90@gmail.com>
2020-06-07 10:44:59 +0530

    Modified the code

--- CleanArchitecture.Infrastructure/Images/Capture.cs
+++ CleanArchitecture.Infrastructure/Images/Capture.cs
@@ -30,12 +30,12 @@ namespace CleanArchitecture.Infrastructure.Images
                 //double height = (byte)managementObject["MaxVerticalImageSize"] / 2.54;
                 //double diagonal = Math.Sqrt(width * width + height * height);
 
-                double width = (byte)managementObject["MaxHorizontalImageSize"] ;
+                double width = (byte)managementObject["MaxHorizontalImageSize"];
                 double height = (byte)managementObject["MaxVerticalImageSize"];
-               // double diagonal = Math.Sqrt(width * width + height * height);
+                double diagonal = Math.Sqrt(width * width + height * height);
 
 
-                Bitmap bmp = new Bitmap(Convert.ToInt32(width * width), Convert.ToInt32(height * height));
+                Bitmap bmp = new Bitmap(Convert.ToInt32(width*width), Convert.ToInt32(height*height));
                 Graphics gr = Graphics.FromImage(bmp);
                 gr.CopyFromScreen(0, 0, 0, 0, bmp.Size);
 
